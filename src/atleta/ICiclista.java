ad6bec7539dedb3fbc5c8bb3332bc96d0df34e8e
jacquelinesantana <ti.jacque@gmail.com>
2023-12-19 12:33:18 -0300

    exemplo de aplicação de interfaces

--- src/atleta/ICiclista.java
+++ src/atleta/ICiclista.java
@@ -1,6 +1,7 @@
 package atleta;
 
 //aqui temos uma interface que obrigará todas as classes que implementar a mesma a utilizar esse método pedalar
+//uma interface tbm pode aceitar extends
 public interface ICiclista  extends IAtleta{
 	public void pedalar();
 }
